887f0405033a6a9172dd93cfdc7cb867c1bebf68
Minh-Tri <minhtri.pham@winnowsolutions.com>
2019-06-12 15:11:00 +0100

    polygon

---
+++ geomt/polygon2d.py
@@ -0,0 +1,10 @@
+'''Polygon in 2D.
+
+A polygon is represented as a collection of 2D points in either clockwise or counter-clockwise order. It is stored in a numpy array of shape (N,2).
+'''
+
+import numpy as _np
+import math as _m
+from .moments2d import EPSILON
+
+
