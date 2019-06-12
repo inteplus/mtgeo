956d66dbc0fe5f3816fe6375b8fed6d32ebfb4e7
Minh-Tri <minhtri.pham@winnowsolutions.com>
2019-06-12 16:30:55 +0100

    unit tests for polygon2d module

--- geomt/polygon2d.py
+++ geomt/polygon2d.py
@@ -24,6 +24,9 @@ def trapezium_integral(poly, func):
         [1] Pham et al. Fast Polygonal Integration and Its Application in Extending Haar-like Features To Improve Object Detection. CVPR, 2010.
         [2] exterior algebra
 
+    :Notes:
+        We use OpenGL's convention here. x-axis points to the right side and y-axis points upward
+
     :Examples:
     >>> from geomt.polygon2d import trapezium_integral
     >>> import numpy as np
@@ -54,30 +57,65 @@ def signed_area(poly):
     >>> from geomt.polygon2d import signed_area
     >>> import numpy as np
     >>> poly = np.array([[10,10],[20,10],[20,20]])
-    >>> signed_area(poly)
+    >>> round(signed_area(poly), 3)
     -50.0
     '''
     return trapezium_integral(poly, lambda x1, y1, x2, y2: 0.5*(x2-x1)*(y1+y2))
 
 
 def moment_x(poly):
-    '''Returns the integral of x over the polygon's interior.'''
+    '''Returns the integral of x over the polygon's interior.
+
+    >>> from geomt.polygon2d import moment_x
+    >>> import numpy as np
+    >>> poly = np.array([[3,4],[2,3],[3,2]])
+    >>> round(moment_x(poly), 3)
+    -2.667
+    '''
     return trapezium_integral(poly, lambda x1, y1, x2, y2: 1/6*(x2-x1)*(x1*(y1*2+y2) + x2*(y1+y2*2)))
 
 def moment_y(poly):
-    '''Returns the integral of y over the polygon's interior.'''
+    '''Returns the integral of y over the polygon's interior.
+
+    >>> from geomt.polygon2d import moment_y
+    >>> import numpy as np
+    >>> poly = np.array([[3,3],[2,2],[3,1]])
+    >>> round(moment_y(poly), 3)
+    -2.0
+    '''
     return trapezium_integral(poly, lambda x1, y1, x2, y2: 1/6*(x2-x1)*(y1*y1 + y1*y2 + y2*y2))
 
 def moment_xy(poly):
-    '''Returns the integral of x*y over the polygon's interior.'''
+    '''Returns the integral of x*y over the polygon's interior.
+
+    >>> from geomt.polygon2d import moment_xy
+    >>> import numpy as np
+    >>> poly = np.array([[3,3],[2,2],[3,1]])
+    >>> round(moment_xy(poly), 3)
+    -5.333
+    '''
     return trapezium_integral(poly, lambda x1, y1, x2, y2: 1/24*(x2-x1)*(y1*y1*(x1*3+x2) + y1*y2*2*(x1+x2) + y2*y2*(x1+x2*3)))
 
 def moment_xx(poly):
-    '''Returns the integral of x*x over the polygon's interior.'''
+    '''Returns the integral of x*x over the polygon's interior.
+
+    >>> from geomt.polygon2d import moment_xx
+    >>> import numpy as np
+    >>> poly = np.array([[3,3],[2,2],[3,1]])
+    >>> round(moment_xx(poly), 3)
+    -7.167
+    '''
     return trapezium_integral(poly, lambda x1, y1, x2, y2: 1/12*(x2-x1)*(x1*x1*(y1*3+y2) + x1*x2*2*(y1+y2) + x2*x2*(y1+y2*3)))
 
 def moment_yy(poly):
-    '''Returns the integral of y*y over the polygon's interior.'''
+    '''Returns the integral of y*y over the polygon's interior.
+
+    >>> from geomt.polygon2d import moment_yy
+    >>> import numpy as np
+    >>> poly = np.array([[3,3],[2,2],[3,1]])
+    >>> round(moment_yy(poly), 3)
+    -4.167
+    '''
     return trapezium_integral(poly, lambda x1, y1, x2, y2: 1/12*(x2-x1)*(y1+y2)*(y1*y1+y2*y2))
 
 
@@ -90,6 +128,22 @@ def to_moments2d(poly):
     :Returns:
         retval : moments2d
             the collection of moments up to 2nd order
+
+    :Examples:
+    >>> from geomt.polygon2d import to_moments2d
+    >>> import numpy as np
+    >>> poly = np.array([[3,3],[2,2],[3,1]])
+    >>> m = to_moments2d(poly)
+    >>> round(m.m0, 3)
+    -1.0
+    >>> round(m.m1.sum(), 3)
+    -4.667
+    >>> round(m.m2.sum(), 3)
+    -22.0
+    >>> round(m.mean.sum(), 3)
+    4.667
+    >>> round(m.cov.sum(), 3)
+    -22.444
     '''
     m0 = signed_area(poly)
     m1 = [moment_x(poly), moment_y(poly)]
