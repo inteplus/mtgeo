616f702c4ce6d7b7215d9e5d071b9c8d79cbc4b6
Minh-Tri Pham <pmtri80@gmail.com>
2020-05-04 11:48:45 +0100

    version.py: renamed version->VERSION

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages, find_namespace_packages
-from mt.geo.version import version
+from mt.geo.version import VERSION
 
 setup(
     name='geomt',
-    version=version,
+    version=VERSION,
     description="The most fundamental geometric modules in Python for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_packages() + find_namespace_packages(include=['mt.*']),
