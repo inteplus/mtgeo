5f1bfc6059f297c8f0c022929c13e0461bf396d3
Minh-Tri Pham <pmtri80@gmail.com>
2020-07-01 13:37:31 +0100

    v0.3.1: supporting cython

--- mt/geo/__init__.py
+++ mt/geo/__init__.py
@@ -1,4 +1,3 @@
-import mt.base.pyximportcpp as pyximport; pyximport.install(language_level=3)
 #from .interval import interval
 from .rect import rect
 from .affine_transformation import *
@@ -7,6 +6,6 @@ from .isometry import *
 from .dilatation import *
 from .similarity2d import *
 from .ellipse import ellipse
-from .linear2d import lin2
+#from .linear2d import lin2
 
 # TODO: doctest for each of the classes above
