f161e593c8cd087c6233eda3c04814565053438a
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-03-21 21:12:34 +0000

    bug fixes

--- mt/geo3d/point.py
+++ mt/geo3d/point.py
@@ -7,16 +7,16 @@ For efficiency reasons, please try to bunch points into arrays or lists and use 
 import numpy as _np
 import mt.base.casting as _bc
 from ..geo.object import ThreeD
-from ..geond import point
+from ..geond.point import Point, castable_ndarray_Point
 
 
 __all__ = ['Point3d']
 
 
-class Point3d(ThreeD, point.Point):
+class Point3d(ThreeD, Point):
     '''A 3D point. See Point for more details.'''
     pass
-_bc.register_castable(_np.ndarray, Point3d, lambda x: point.castable_ndarray_Point(x, 3))
+_bc.register_castable(_np.ndarray, Point3d, lambda x: castable_ndarray_Point(x, 3))
 _bc.register_cast(_np.ndarray, Point3d, lambda x: Point3d(x, check=False))
 _bc.register_cast(Point3d, Point, lambda x: Point(x.point, check=False))
 _bc.register_cast(Point, Point3d, lambda x: Point3d(x.point, check=False))
