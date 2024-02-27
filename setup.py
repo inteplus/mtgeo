f6c7598308e482b5ec02f257f500b030f786f335
Minh-Tri <minhtri.pham@winnowsolutions.com>
2024-02-27 14:56:24 +0000

    updated version.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_packages, find_namespace_packages
-from mt.geo.version import version
+
+VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
 
 setup(
     name="mtgeo",
-    version=version,
     description="The most fundamental geometric modules in Python for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_packages() + find_namespace_packages(include=["mt.*"]),
@@ -21,5 +22,14 @@ setup(
     project_urls={
         "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.geo/mt.geo.html",
         "Source Code": "https://github.com/inteplus/mtgeo",
+    },
+    setup_requires=["setuptools-git-versioning<2"],
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_file": VERSION_FILE,
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}+{branch}",
+        "dirty_template": "{tag}.post{ccount}",
     },
 )
