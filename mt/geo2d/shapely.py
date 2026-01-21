6f3005f37d28280730fc3eebc1df0ad401854d71
Minh-Tri Pham <pmtri80@gmail.com>
2026-01-21 05:23:35 +0000

    updated version.py to 1.1.15

--- mt/geo2d/shapely.py
+++ mt/geo2d/shapely.py
@@ -1,24 +1,25 @@
-'''Support for shapely.'''
+"""Support for shapely."""
 
-
-__all__ = ['HasShapely', 'join_volume_shapely']
+__all__ = ["HasShapely", "join_volume_shapely"]
 
 
 class HasShapely(object):
-    '''Mixin to assert that the geometric object has the 'shapely' property.'''
+    """Mixin to assert that the geometric object has the 'shapely' property."""
 
     @property
     def shapely(self):
-        '''Shapely representation for fast intersection operations. Noe that shapely treats
-        signed and unsigned regions equally.'''
+        """Shapely representation for fast intersection operations. Noe that shapely treats
+        signed and unsigned regions equally."""
         raise NotImplementedError("Implement me.")
 
 
 # ----- joining volumes -----
 
 
-def join_volume_shapely(obj1: HasShapely, obj2: HasShapely):
-    '''Joins the areas of two 2D geometry objects supporting shapely.
+def join_volume_shapely(
+    obj1: HasShapely, obj2: HasShapely, intersection_grid_size=None
+):
+    """Joins the areas of two 2D geometry objects supporting shapely.
 
     Parameters
     ----------
@@ -26,6 +27,8 @@ def join_volume_shapely(obj1: HasShapely, obj2: HasShapely):
         the first 2D geometry object
     obj2 : HasShapely
         the second 2D geometry object
+    intersection_grid_size : float or None
+        grid size for approximating intersection area. If None, exact computation is used.
 
     Returns
     -------
@@ -37,9 +40,16 @@ def join_volume_shapely(obj1: HasShapely, obj2: HasShapely):
         the area of the interior of obj2 that does not belong to obj1
     union_area : float
         the area of the union of the two objects' interior regions
-    '''
+    """
 
-    inter_area = obj1.shapely.intersection(obj2.shapely).area
+    inter_area = obj1.shapely.intersection(
+        obj2.shapely, grid_size=intersection_grid_size
+    ).area
     obj1_area = obj1.shapely.area
     obj2_area = obj2.shapely.area
-    return (inter_area, obj1_area - inter_area, obj2_area - inter_area, obj1_area + obj2_area - inter_area)
+    return (
+        inter_area,
+        obj1_area - inter_area,
+        obj2_area - inter_area,
+        obj1_area + obj2_area - inter_area,
+    )
